9e91ca10af12a6634b84433d0b6c089641a2a08c
zaky <ahmadzakyarifin.com>
2026-06-10 21:45:24 +0700

    chore: update pemanggilan koneksi database di main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "github.com/ahmadzakyarifin/tatadana/config"
+import (
+	"github.com/ahmadzakyarifin/tatadana/config"
+	infrastructure "github.com/ahmadzakyarifin/tatadana/internal/Infrastructure"
+)
 
 func main(){
 	cfg := config.LoadConfig()
-	
+	_ = infrastructure.ConnectDB(cfg)
 }
